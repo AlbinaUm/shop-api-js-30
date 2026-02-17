1eca8a944f950a01ae6de9c994581e3e01e7e29c
Umarova Albina <albinaumarova2014@gmail.com>
2026-02-17 20:09:49 +0600

    Added cord lib

--- index.ts
+++ index.ts
@@ -1,31 +1,15 @@
 import express from 'express'
 import productsRouter from "./routes/products";
 import fileDb from "./fileDb";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/products', productsRouter);
-
-app.get('/:message', (req: express.Request, res: express.Response) => {
-    res.send(req.params.message);
-});
-
-const Vigenere = require('caesar-salad').Vigenere;
-const password = 123;
-
-app.get('/encode/:message', (req: express.Request, res: express.Response) => {
-    const message = Vigenere.Cipher(password).crypt(req.params.message);
-    res.send(message);
-});
-
-app.get('/decode/:secretMessage', (req: express.Request, res: express.Response) => {
-    const message = Vigenere.Decipher(password).crypt(req.params.secretMessage);
-    res.send(message);
-});
-
 
 const run = async () => {
     await fileDb.init();
